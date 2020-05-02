e1873edb3b6bc719647d92912c4e48ccb9fbdba7
Eshi44 <e.hameliervolino@gmail.com>
2020-05-02 15:15:59 -0400

    installed npm typewritter effect and added rules and effect to main page

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,7 +1,28 @@
+var typewriterone = document.getElementById("typewriterone");
 $(document).ready(() => {
   $(document).foundation();
   $("#next-page").on("click", () => {
     location.href = "/pokemon";
     return false;
   });
+
+  var type = new Typewriter(typewriterone, {
+    loop: false,
+    delay: 60,
+  });
+
+  type
+    .pauseFor(1500)
+    .typeString("Welcome all trainers!")
+    .typeString("<br>")
+    .pauseFor(1500)
+    .typeString("<br> Have you ever had a dispute between friends that ended in disaster?")
+    .typeString("<br>")
+    .pauseFor(1500)
+    .typeString("<br> From calling 'shotgun' to claiming the last slice of pizza, PokéBattle is for you!")
+    .typeString("<br>")
+    .pauseFor(1500)
+    .typeString("<br> Enter the arena, generate your pokemon, and battle to see who wins!")
+    .start();
+
 });
